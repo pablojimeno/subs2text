b24b3328a5681e95d83f918bdeb7e6eb10a0b0ac
pablojimeno <pablo@spines.me>
2014-11-13 15:10:27 +0100

    First version of substitle to text ruby converter

---
+++ converter.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+subs = "subtitle.srt"
+
+@regex = /\d\d:\d\d,\d\d\d -->/
+lines = Array.new
+
+IO.readlines(subs).each_with_index do |line, idx|
+  if idx > 1
+	lines = lines.push(line) 
+  	if line.match(@regex)
+  	  3.times { lines.pop }
+    end
+  end
+end
+
+
+text = File.open("output.txt", "w") do |f|
+  lines.each do |l|
+    f.puts l
+  end
+end
